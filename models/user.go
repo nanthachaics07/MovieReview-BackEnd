223f393eb0c8db99eec5f43c7aaae5a7a8512439
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-24 14:22:10 +0700

    DEV: Fix User struct

--- models/user.go
+++ models/user.go
@@ -1,9 +1,25 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	ID                uint      `json:"id" gorm:"primaryKey"`
+	Name              string    `json:"name"`
+	Email             string    `json:"email" gorm:"unique"`
+	Password          []byte    `json:"-"`
+	AdminRole         uint      `json:"adminRole"`
+	BusinessRole      uint      `json:"businessRole"`
+	ResidentialRole   uint      `json:"residentialRole"`
+	LastModified      time.Time `json:"lastModified"`
+	LastLoginAttempt  time.Time `json:"lastLoginAttempt"`
+	LoginAttemptCount uint      `json:"loginAttemptCount"`
+	Blocked           uint      `json:"blocked"`
+	PasswordPolicyId  uint      `json:"-" gorm:"default:1;foreignKey:id"`
+	LoginPolicyId     uint      `json:"-" gorm:"default:1;foreignKey:id"`
+	PasswordHistoryId uint      `json:"-" gorm:"default:1;foreignKey:id"`
 }
