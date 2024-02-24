effe5616e1bb9c1d21681a0f7a6d7b3382d2eef7
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-24 16:16:52 +0700

    DEV: Fix Auth Check Token

--- authentication/auth.go
+++ authentication/auth.go
@@ -2,30 +2,85 @@ package authentication
 
 import (
 	"fmt"
-	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt"
+	"github.com/wpcodevo/golang-fiber-jwt/initializers"
+	"github.com/wpcodevo/golang-fiber-jwt/models"
 )
 
-func AuthenticationRequired(c *fiber.Ctx) error {
-	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{},
-		func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("JWT_SECRET_KEY")), nil
-		})
-	if err != nil || !token.Valid {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
-	}
-	claims, ok := token.Claims.(*jwt.MapClaims)
-	if !ok {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
+func DeserializeRequiresAuth(c *fiber.Ctx) error {
+	var tokenString string
+	authorization := c.Get("Authorization")
+
+	if strings.HasPrefix(authorization, "Bearer ") {
+		tokenString = strings.TrimPrefix(authorization, "Bearer ")
+	} else if c.Cookies("token") != "" {
+		tokenString = c.Cookies("token")
 	}
 
-	fmt.Println(claims)
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "fail", "message": "You are not logged in"})
+	}
+
+	config, _ := initializers.LoadConfig(".")
+
+	tokenByte, err := jwt.Parse(tokenString, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", jwtToken.Header["alg"])
+		}
+
+		return []byte(config.JwtSecret), nil
+	})
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "fail", "message": fmt.Sprintf("invalidate token: %v", err)})
+	}
+
+	claims, ok := tokenByte.Claims.(jwt.MapClaims)
+	if !ok || !tokenByte.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "fail", "message": "invalid token claim"})
+
+	}
+
+	var user models.User
+	initializers.DB.First(&user, "id = ?", fmt.Sprint(claims["sub"]))
+
+	if user.ID.String() != claims["sub"] {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"status": "fail", "message": "the user belonging to this token no logger exists"})
+	}
+
+	c.Locals("user", models.FilterUserRecord(&user))
+
 	return c.Next()
 }
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/golang-jwt/jwt"
+// )
+
+// func AuthenticationRequired(c *fiber.Ctx) error {
+// 	cookie := c.Cookies("jwt")
+// 	token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{},
+// 		func(token *jwt.Token) (interface{}, error) {
+// 			return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+// 		})
+// 	if err != nil || !token.Valid {
+// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 			"message": "Unauthorized",
+// 		})
+// 	}
+// 	claims, ok := token.Claims.(*jwt.MapClaims)
+// 	if !ok {
+// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 			"message": "Unauthorized",
+// 		})
+// 	}
+
+// 	fmt.Println(claims)
+// 	return c.Next()
+// }
--- router/router.go
+++ router/router.go
@@ -1,16 +1,25 @@
 package router
 
 import (
-	_ "MovieReviewAPIs/authentication"
+	"MovieReviewAPIs/authentication"
 	"MovieReviewAPIs/handler"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func Router(app *fiber.App) {
-	app.Get("/api/movies", handler.GetAllMovies)
-	app.Get("/api/movies/:id", handler.GetMovieByID)
-	app.Post("/api/movies", handler.CreateMovie)
-	app.Put("/api/movies/:id", handler.UpdateMovie)
-	app.Delete("/api/movies/:id", handler.DeleteMovie)
+func Router(app *fiber.App, handler *handler.MovieHandler) {
+	auth := app.Group("/api", authentication.DeserializeRequiresAuth)
+
+	app.Get("/movies", handler.GetAllMovies)
+
+	auth.Get("/movies/:id", handler.GetMovieByID)
+	auth.Post("/movies", handler.CreateMovie)
+	auth.Put("/movies/:id", handler.UpdateMovie)
+	auth.Delete("/movies/:id", handler.DeleteMovie)
+
+	// app.Get("/api/movies", handler.GetAllMovies)
+	// app.Get("/api/movies/:id", handler.GetMovieByID)
+	// app.Post("/api/movies", handler.CreateMovie)
+	// app.Put("/api/movies/:id", handler.UpdateMovie)
+	// app.Delete("/api/movies/:id", handler.DeleteMovie)
 }
