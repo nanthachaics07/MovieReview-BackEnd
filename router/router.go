9327e43310daeaa59a9dfae7424f6e3523cf74ca
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-24 23:04:36 +0700

    DEV: Router

--- router/router.go
+++ router/router.go
@@ -8,6 +8,7 @@ import (
 )
 
 func MovieRouter(app *fiber.App, Mhandler *handler.MovieHandler) {
+
 	app.Get("/movies", Mhandler.GetAllMovies)
 
 	auth := app.Group("/api", authentication.DeserializeRequiresAuth)
@@ -22,4 +23,14 @@ func MovieRouter(app *fiber.App, Mhandler *handler.MovieHandler) {
 	// app.Post("/api/movies", handler.CreateMovie)
 	// app.Put("/api/movies/:id", handler.UpdateMovie)
 	// app.Delete("/api/movies/:id", handler.DeleteMovie)
+}
+
+func UserRouter(app *fiber.App, Uhandler *handler.UserHandler) {
+
+	auth := app.Group("/api", authentication.DeserializeRequiresAuth)
+
+	auth.Post("/register", Uhandler.RegisterUser)
+	auth.Post("/login", Uhandler.LoginUser)
+	auth.Post("/logout", Uhandler.LogoutUser)
+
 }
